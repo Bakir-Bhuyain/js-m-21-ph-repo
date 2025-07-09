e7faaae5b4c49a93630021545109f5941b700268
Bakir Bhuyain <hemalbakir@gmail.com>
2025-07-09 23:50:55 +0600

    Add more swap examples with variables and destructuring

--- swap.js
+++ swap.js
@@ -13,3 +13,18 @@ let y = 7;
 console.log(x, y);
 [x,y] = [y,x];
 console.log(x,y);
+
+
+let u = 10;
+let o = 30;
+console.log(u, o);
+const emp = u;
+u = o;
+o = emp;
+console.log(u, o);
+
+let g = 90;
+let h = 25;
+console.log(g, h);
+[g, h] = [h, g];
+console.log(g, h);
